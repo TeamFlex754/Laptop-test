236ceef41c15a4beea5a3e9cac101e96a327f6a4
TeamFlex754 <ians.mcvay@gmail.com>
2024-08-15 18:35:30 -0700

    First commit

---
+++ src/CTWeek1.java
@@ -0,0 +1,34 @@
+/*Pseudocode:
+
+List individual's name and address information:
+
+String userFirstName = John
+String userLastName = Smith
+String streetAddress = 734 Maple Ln
+String city = San Diego
+String zipCode = 82323
+
+Print = userFirstName
+Print = userLastName
+Print = streetAddress
+Print = city
+Print = zipCode
+ */
+
+
+public class CTWeek1 {
+    public static void main(String[] args) {
+
+        String userFirstName = "John";
+        String userLastName = "Smith";
+        String streetAddress = "734 Maple Ln";
+        String city = "San Diego";
+        String zipCode = "82323";
+
+        System.out.println(userFirstName);
+        System.out.println(userLastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
